a19bfc9f7f8377fd6ad9b0b6c43d85f59a45e04f
Tobi Saputra <tobisaputra11@gmail.com>
2026-05-10 13:03:17 +0700

    refactor(proto): migrate to ES module syntax and remove package.json

--- WAProto/index.js
+++ WAProto/index.js
@@ -1,33 +1,57 @@
-const proto = {
-	...require('./WAAdv').WAAdv,
-	...require('./WAAICommon').WAAICommon,
-	...require('./WAAICommonDeprecated').WAAICommonDeprecated,
-	...require('./WACert').WACert,
-	...require('./WAChatLockSettings').WAChatLockSettings,
-	...require('./WACommon').WACommon,
-	...require('./WACompanionReg').WACompanionReg,
-	...require('./WADeviceCapabilities').WADeviceCapabilities,
-	...require('./WAE2E').WAE2E,
-	...require('./WAEphemeral').WAEphemeral,
-	...require('./WAHistorySync').WAHistorySync,
-	...require('./WALidMigrationSyncPayload').WALidMigrationSyncPayload,
-	...require('./WAMdStorageAccountLinkingOpaqueData').WAMdStorageAccountLinkingOpaqueData,
-	...require('./WAMdStorageChatRowOpaqueData').WAMdStorageChatRowOpaqueData,
-	...require('./WAMdStorageMsgRowOpaqueData').WAMdStorageMsgRowOpaqueData,
-	...require('./WAMmsRetry').WAMmsRetry,
-	...require('./WAQuickPromotionSurfaces').WAQuickPromotionSurfaces,
-	...require('./WAReporting').WAReporting,
-	...require('./WAServerSync').WAServerSync,
-	...require('./WASignalLocalStorageProtocol').WASignalLocalStorageProtocol,
-	...require('./WASignalWhisperTextProtocol').WASignalWhisperTextProtocol,
-	...require('./WAStatusAttributions').WAStatusAttributions,
-	...require('./WASyncAction').WASyncAction,
-	...require('./WAUserPassword').WAUserPassword,
-	...require('./WAVnameCert').WAVnameCert,
-	...require('./WAWa6').WAWa6,
-	...require('./WAWeb').WAWeb,
-}
+import { WAAdv } from './WAAdv.js';
+import { WAAICommon } from './WAAICommon.js';
+import { WAAICommonDeprecated } from './WAAICommonDeprecated.js';
+import { WACert } from './WACert.js';
+import { WAChatLockSettings } from './WAChatLockSettings.js';
+import { WACommon } from './WACommon.js';
+import { WACompanionReg } from './WACompanionReg.js';
+import { WADeviceCapabilities } from './WADeviceCapabilities.js';
+import { WAE2E } from './WAE2E.js';
+import { WAEphemeral } from './WAEphemeral.js';
+import { WAHistorySync } from './WAHistorySync.js';
+import { WALidMigrationSyncPayload } from './WALidMigrationSyncPayload.js';
+import { WAMdStorageAccountLinkingOpaqueData } from './WAMdStorageAccountLinkingOpaqueData.js';
+import { WAMdStorageChatRowOpaqueData } from './WAMdStorageChatRowOpaqueData.js';
+import { WAMdStorageMsgRowOpaqueData } from './WAMdStorageMsgRowOpaqueData.js';
+import { WAMmsRetry } from './WAMmsRetry.js';
+import { WAQuickPromotionSurfaces } from './WAQuickPromotionSurfaces.js';
+import { WAReporting } from './WAReporting.js';
+import { WAServerSync } from './WAServerSync.js';
+import { WASignalLocalStorageProtocol } from './WASignalLocalStorageProtocol.js';
+import { WASignalWhisperTextProtocol } from './WASignalWhisperTextProtocol.js';
+import { WAStatusAttributions } from './WAStatusAttributions.js';
+import { WASyncAction } from './WASyncAction.js';
+import { WAUserPassword } from './WAUserPassword.js';
+import { WAVnameCert } from './WAVnameCert.js';
+import { WAWa6 } from './WAWa6.js';
+import { WAWeb } from './WAWeb.js';
 
-module.exports = {
-  proto
+export const proto = {
+	...WAAdv,
+	...WAAICommon,
+	...WAAICommonDeprecated,
+	...WACert,
+	...WAChatLockSettings,
+	...WACommon,
+	...WACompanionReg,
+	...WADeviceCapabilities,
+	...WAE2E,
+	...WAEphemeral,
+	...WAHistorySync,
+	...WALidMigrationSyncPayload,
+	...WAMdStorageAccountLinkingOpaqueData,
+	...WAMdStorageChatRowOpaqueData,
+	...WAMdStorageMsgRowOpaqueData,
+	...WAMmsRetry,
+	...WAQuickPromotionSurfaces,
+	...WAReporting,
+	...WAServerSync,
+	...WASignalLocalStorageProtocol,
+	...WASignalWhisperTextProtocol,
+	...WAStatusAttributions,
+	...WASyncAction,
+	...WAUserPassword,
+	...WAVnameCert,
+	...WAWa6,
+	...WAWeb,
 }
